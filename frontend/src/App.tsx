73a3cd51760e6c21442dd8448600893ed4a564fe
Washington Albuquerque Moura <walbuque@gmail.com>
2021-09-06 17:54:26 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,20 @@
-
-
+import DataTable from "components/Datatable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
 
 function App() {
   return (
-    <div>
-     <h1 className="text-primary">Hello my friend!</h1> 
-    </div>
+    <>
+
+      <NavBar />
+      <div className = "container">
+        <h1 className="text-primary">Hello my friend!</h1>
+
+        <DataTable />
+
+      </div>
+      <Footer />
+    </>
   );
 }
 
