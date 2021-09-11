77e9262e3ca7578401d567810b2ab2bd55bbadd2
Washington Albuquerque Moura <walbuque@gmail.com>
2021-09-11 13:10:26 -0300

    first homolog

--- backend/src/main/java/com/estudos/dsvendas/repositories/SaleRepository.java
+++ backend/src/main/java/com/estudos/dsvendas/repositories/SaleRepository.java
@@ -1,9 +1,20 @@
 package com.estudos.dsvendas.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.estudos.dsvendas.dto.SaleSuccessDTO;
+import com.estudos.dsvendas.dto.SaleSumDTO;
 import com.estudos.dsvendas.entities.Sale;
 
 public interface SaleRepository extends JpaRepository <Sale, Long> {
+	
+	@Query("SELECT new com.estudos.dsvendas.dto.SaleSumDTO(obj.seller, SUM(obj.amount) FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSumDTO> amountGroupedBySeller();
+	
+	@Query("SELECT new com.estudos.dsvendas.dto.SaleSuccessDTO(obj.seller, SUM(obj.visited, SUM(obj.deals) FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSuccessDTO> successGroupedBySeller();
 
 }
