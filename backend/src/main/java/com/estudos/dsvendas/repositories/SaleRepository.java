e55c94a2734804e6121777bbd3fe81ef39dfbefe
Washington Albuquerque Moura <walbuque@gmail.com>
2021-09-12 13:19:47 -0300

    group by search

--- backend/src/main/java/com/estudos/dsvendas/repositories/SaleRepository.java
+++ backend/src/main/java/com/estudos/dsvendas/repositories/SaleRepository.java
@@ -11,10 +11,10 @@ import com.estudos.dsvendas.entities.Sale;
 
 public interface SaleRepository extends JpaRepository <Sale, Long> {
 	
-	@Query("SELECT new com.estudos.dsvendas.dto.SaleSumDTO(obj.seller, SUM(obj.amount) FROM Sale AS obj GROUP BY obj.seller")
+	@Query("SELECT new com.estudos.dsvendas.dto.SaleSumDTO(obj.seller, SUM(obj.amount)) FROM Sale AS obj GROUP BY obj.seller")
 	List<SaleSumDTO> amountGroupedBySeller();
 	
-	@Query("SELECT new com.estudos.dsvendas.dto.SaleSuccessDTO(obj.seller, SUM(obj.visited, SUM(obj.deals) FROM Sale AS obj GROUP BY obj.seller")
+	@Query("SELECT new com.estudos.dsvendas.dto.SaleSuccessDTO(obj.seller, SUM(obj.visited), SUM(obj.deals)) FROM Sale AS obj GROUP BY obj.seller")
 	List<SaleSuccessDTO> successGroupedBySeller();
 
 }
