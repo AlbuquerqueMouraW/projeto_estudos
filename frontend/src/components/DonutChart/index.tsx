86871aa744a3f34e1cd95172f4b941d437803a3d
Washington Albuquerque Moura <walbuque@gmail.com>
2021-09-12 16:23:19 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'Types/Sale';
 import { Base_URL } from 'utils/requests';
@@ -8,20 +9,22 @@ type ChartData ={
     series: number[];
 }
 
-function DonutChart() {
+const DonutChart = () => {
 
-    let chartData: ChartData = {labels: [], series: [] };
+    const [chartData, setChartData] = useState<ChartData>({labels: [], series: [] });
 
-   axios.get(`${Base_URL}/sales/amount-by-seller`)
+    useEffect(() =>{
+        axios.get(`${Base_URL}/sales/amount-by-seller`)
         .then(response => {
             const data = response.data as SaleSum[];
             const myLabels = data.map(x => x.sellerName);
             const mySeries = data.map(x => x.sum);
 
-            chartData = {labels: myLabels, series: mySeries };
-            console.log(chartData);
-        } )
+            setChartData = ({ labels: myLabels, series: mySeries });
+        } );
+    } , [] );
 
+    
    // const mockData = {
    //     series: [477138, 499928, 444867, 220426, 473088],
    //     labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
